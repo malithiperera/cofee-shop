67bd2e0e622eca65b09a6a6f7b1d2800ab6b3835
malithiperera <malithiperera1998@gmail.com>
2022-07-13 08:55:04 +0530

    component create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from '../src/Components/Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import About from './Components/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+<Navbar />
+    <Routes>
+   
+     <Route exact path="/" element={<Home />}/>
+     <Route exact path="/about" element={<About />}/>
+    </Routes>
+
+</Router>
+   
   );
 }
 
---
+++ src/Components/Products.js
@@ -0,0 +1,7 @@
+const Product = () => {
+    return ( 
+        <h2>Products</h2>
+     );
+}
+ 
+export default Product;
